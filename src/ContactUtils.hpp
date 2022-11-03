fb6d8f284cdd76349510c2ff4892d6d4386086e3
ralberd <ralberd@sandia.gov>
2022-11-03 16:08:07 -0600

    pulling out repeated code for getting parent elements of a contact pair side

--- src/ContactUtils.hpp
+++ src/ContactUtils.hpp
@@ -70,6 +70,25 @@ Plato::OrdinalVector convert_to_elementwise_map
 Plato::OrdinalType count_total_child_nodes(const std::vector<ContactPair> & aPairs);
 
 template<typename ElementType>
+Plato::OrdinalVector find_parent_elements_for_side
+(const Plato::OrdinalVectorT<const Plato::OrdinalType> & aChildNodes,
+ const std::string                                     & aParentBlock,
+ const Teuchos::Array<Plato::Scalar>                   & aTranslation,
+ const Plato::SpatialModel                             & aSpatialModel,
+       Plato::Scalar                                     aScale = 1.0)
+{
+    auto tChildLocations       = compute_node_locations(aSpatialModel.Mesh, aChildNodes);
+    auto tMappedChildLocations = map_node_locations(tChildLocations, aTranslation, aScale);
+    Plato::SpatialDomain tDomain = get_domain(aParentBlock, aSpatialModel.Domains);
+
+    Plato::OrdinalVector tParentElements("parent elements", aChildNodes.size());
+    Plato::Geometry::findParentElements<ElementType, Plato::Scalar>
+    (aSpatialModel.Mesh, tDomain.cellOrdinals(), tChildLocations, tMappedChildLocations, tParentElements);
+
+    return tParentElements;
+}
+
+template<typename ElementType>
 void populate_full_contact_arrays
 (const std::vector<ContactPair> & aPairs,
  const Plato::SpatialModel      & aSpatialModel,
@@ -79,37 +98,21 @@ void populate_full_contact_arrays
     Plato::OrdinalType tOffset(0);
     for (auto tPair : aPairs)
     {
-        // side A parents
+        auto tParentElements = find_parent_elements_for_side<ElementType>(tPair.childNodesA, tPair.parentBlockB, tPair.initialGap, aSpatialModel);
         Plato::OrdinalType tNumNodes = tPair.childNodesA.size();
-        auto tChildLocations       = compute_node_locations(aSpatialModel.Mesh, tPair.childNodesA);
-        auto tMappedChildLocations = map_node_locations(tChildLocations, tPair.initialGap);
-        Plato::SpatialDomain tDomain = get_domain(tPair.parentBlockB, aSpatialModel.Domains);
-
-        Plato::OrdinalVector tParentElementsA("parent elements", tNumNodes);
-        Plato::Geometry::findParentElements<ElementType, Plato::Scalar>
-        (aSpatialModel.Mesh, tDomain.cellOrdinals(), tChildLocations, tMappedChildLocations, tParentElementsA);
-
         Kokkos::parallel_for(Kokkos::RangePolicy<Plato::OrdinalType>(0,tNumNodes), KOKKOS_LAMBDA(Plato::OrdinalType nodeOrdinal)
         {
             aChildNodes(tOffset + nodeOrdinal) = tPair.childNodesA(nodeOrdinal);
-            aParentElements(tOffset + nodeOrdinal) = tParentElementsA(nodeOrdinal);
+            aParentElements(tOffset + nodeOrdinal) = tParentElements(nodeOrdinal);
         }, "store parent elements");
         tOffset += tNumNodes;
 
-        // side B parents
+        tParentElements = find_parent_elements_for_side<ElementType>(tPair.childNodesB, tPair.parentBlockA, tPair.initialGap, aSpatialModel, -1.0);
         tNumNodes = tPair.childNodesB.size();
-        tChildLocations       = compute_node_locations(aSpatialModel.Mesh, tPair.childNodesB);
-        tMappedChildLocations = map_node_locations(tChildLocations, tPair.initialGap, -1.0);
-        tDomain = get_domain(tPair.parentBlockA, aSpatialModel.Domains);
-
-        Plato::OrdinalVector tParentElementsB("parent elements", tNumNodes);
-        Plato::Geometry::findParentElements<ElementType, Plato::Scalar>
-        (aSpatialModel.Mesh, tDomain.cellOrdinals(), tChildLocations, tMappedChildLocations, tParentElementsB);
-
         Kokkos::parallel_for(Kokkos::RangePolicy<Plato::OrdinalType>(0,tNumNodes), KOKKOS_LAMBDA(Plato::OrdinalType nodeOrdinal)
         {
             aChildNodes(tOffset + nodeOrdinal) = tPair.childNodesB(nodeOrdinal);
-            aParentElements(tOffset + nodeOrdinal) = tParentElementsB(nodeOrdinal);
+            aParentElements(tOffset + nodeOrdinal) = tParentElements(nodeOrdinal);
         }, "store parent elements");
         tOffset += tNumNodes;
     }
