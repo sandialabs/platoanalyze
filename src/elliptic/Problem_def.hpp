73fb72ec8580ddd917199bab3fcc3861dab1515b
Joshua Robbins <jrobbin@sandia.gov>
2022-11-23 16:00:59 -0700

    added radiation bc test. Fixed Newton iteration.

--- src/elliptic/Problem_def.hpp
+++ src/elliptic/Problem_def.hpp
@@ -203,6 +203,11 @@ namespace Elliptic
             mResidual = mPDE->value(tStatesSubView, aControl);
             Plato::blas1::scale(-1.0, mResidual);
 
+            mJacobian = mPDE->gradient_u(tStatesSubView, aControl);
+
+            Plato::Scalar tScale = (tNewtonIndex == 0) ? 1.0 : 0.0;
+            this->applyStateConstraints(mJacobian, mResidual, tScale);
+
             if (mNumNewtonSteps > 1) {
                 auto tResidualNorm = Plato::blas1::norm(mResidual);
                 std::cout << " Residual norm: " << tResidualNorm << std::endl;
@@ -211,11 +216,6 @@ namespace Elliptic
                     break;
                 }
             }
-
-            mJacobian = mPDE->gradient_u(tStatesSubView, aControl);
-
-            Plato::Scalar tScale = (tNewtonIndex == 0) ? 1.0 : 0.0;
-            this->applyStateConstraints(mJacobian, mResidual, tScale);
 
             Plato::ScalarVector tDeltaD("increment", tStatesSubView.extent(0));
             Plato::blas1::fill(static_cast<Plato::Scalar>(0.0), tDeltaD);
